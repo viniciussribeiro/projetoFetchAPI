a0343bd1166639e658029a9421be30e45ea65ec0
viniciussribeiro <vini.sribeiro@hotmail.com>
2021-11-23 08:11:03 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,76 @@
+window.onload = function() {
+   const cadastrar = document.querySelector("#cadastrar");
+   const nome = document.querySelector("#nome");
+   const curso = document.querySelector("#curso");
+   const buscar = document.querySelector("#buscar");
+   const id = document.querySelector("#id");
+   const alterar = document.querySelector("#alterar");
+   const deletar = document.querySelector("#deletar");
+
+   //ação de cadastrar pessoa e curso;
+   cadastrar.addEventListener('click', function() {
+      let formdata = new formdata();
+      formdata.append('nome', `${nome.value}`);
+      formdata.append('curso', `${curso.value}`);
+
+      fetch("https://www.jussimarleal.com.br/exemplo_api/pessoa",
+      {
+        body: formdata, 
+        method: 'post',
+        mode: 'cors',
+        cache: 'default'
+      }).then(()=> {
+            alert("Registro efetuado com sucesso!");
+            limparCampos();
+      }  
+   );
+   });
+   //metpdo para listar pessoas
+   buscar.addEventListener('click', function() {
+      fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`, {
+        method: 'get',
+        mode: 'cors',
+        cache: 'default'
+      }).then(response=> {
+        response.json().then(data=> {
+          nome.value = data['nome'];
+          curso.value = data['curso'];
+        })
+   })
+   })
+   //método para alterar os dados do registro
+   alterar.addEventListener('click', function() {
+        fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`, {
+        method: 'put',
+        mode: 'cors',
+        cache: 'default',
+        headers: {
+          'Content-type': 'application/json; charset = UTF-8'
+        },
+        body:JSON.stringify({
+           'nome': `${nome.value}`,
+           'curso': `${curso.value}`
+        })
+      }).then(()=>{
+        alert("Registro alterado com sucesso!")
+        limparCampos();
+        });
+   });
+   //método para deletar um registro
+      deletar.addEventListener('click', function() {
+          fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`, {
+           method: 'delete',
+           mode: 'cors',
+          cache: 'default',      
+          }).then(()=>{
+            alert("Registro alterado com sucesso!")
+            limparCampos();
+            });
+      });
+   //metodo para limpar os campos
+   function limparCampos() {
+     nome.value = "";
+     curso.value = "";
+
+   }
+};
